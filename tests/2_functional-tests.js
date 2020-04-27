73d9a36eaac4f89f583835b9891719a5ba3f429a
StaxGit <lario.free@gmail.com>
2020-04-27 11:20:20 +0200

    Add functional test - 2 stocks request

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,62 +14,76 @@ var server = require('../app.js');
 chai.use(chaiHttp);
 
 let googLikes;
+let msftLikes;
 
-suite('Functional Tests', function() {
-    
-    suite('GET /api/stock-prices => stockData object', function() {
-      
-      test('1 stock', function(done) {
-       chai.request(server)
+suite('Functional Tests', function () {
+
+  suite('GET /api/stock-prices => stockData object', function () {
+
+    test('1 stock', function (done) {
+      chai.request(server)
         .get('/api/stock-prices')
-        .query({stock: 'goog'})
-        .end(function(err, res){
+        .query({ stock: 'goog' })
+        .end(function (err, res) {
           assert.equal(res.status, 200);
           assert.equal(typeof res.body, "object");
           assert.equal(res.body.stockData.stock, "GOOG");
           assert.isAtLeast(res.body.stockData.likes, 0);
-          assert.isAtLeast(res.body.stockData.price, 0);        
+          assert.isAtLeast(res.body.stockData.price, 0);
           done();
         });
-      });
-      
-      test('1 stock with like', function(done) {
-        chai.request(server)
+    });
+
+    test('1 stock with like', function (done) {
+      chai.request(server)
         .get("/api/stock-prices")
-        .query({stock: "goog", likes: "true"})
-        .end(function(err, res) {
+        .query({ stock: "goog", likes: "true" })
+        .end(function (err, res) {
           assert.equal(res.status, 200);
           assert.equal(typeof res.body, "object");
           assert.equal(res.body.stockData.stock, "GOOG");
           assert.isAtLeast(res.body.stockData.likes, 1);
           assert.isAtLeast(res.body.stockData.price, 0);
-          googLikes = res.body.stockData.likes;        
+          googLikes = res.body.stockData.likes;
           done();
         })
-      });
-      
-      test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        chai.request(server)
+    });
+
+    test('1 stock with like again (ensure likes arent double counted)', function (done) {
+      chai.request(server)
         .get("/api/stock-prices")
-        .query({stock: "goog", likes: "true"})
-        .end(function(err, res) {
+        .query({ stock: "goog", likes: "true" })
+        .end(function (err, res) {
           assert.equal(res.status, 200);
           assert.equal(typeof res.body, "object");
           assert.equal(res.body.stockData.stock, "GOOG");
           assert.equal(res.body.stockData.likes, googLikes);
-          assert.isAtLeast(res.body.stockData.price, 0);     
+          assert.isAtLeast(res.body.stockData.price, 0);
           done();
         })
-      });
-      
-      test('2 stocks', function(done) {
-        
-      });
-      
-      test('2 stocks with like', function(done) {
-        
-      });
-      
     });
 
-});
+    test('2 stocks', function (done) {
+      chai.request(server)
+        .get("/api/stock-prices")
+        .query({ stock: ["goog", "msft"] })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.stockData[0].stock, "GOOG");
+          assert.exists(res.body.stockData[0].rel_likes, "Not null or undefined");
+          assert.isAtLeast(res.body.stockData[0].price, 0);
+          assert.equal(res.body.stockData[1].stock, "MSFT");
+          assert.exists(res.body.stockData[1].rel_likes, "Not null or undefined");
+          assert.isAtLeast(res.body.stockData[1].price, 0);
+          msftLikes = res.body.stockData[1].likes;
+          done()
+        });
+      });
+
+      test('2 stocks with like', function (done) {
+
+      });
+
+    });
+
+  });
